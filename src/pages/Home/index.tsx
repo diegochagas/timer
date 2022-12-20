fbbaadcee307d0841bfd770c327271d735eb8566
Diego Chagas <drochachagas@gmail.com>
2022-12-20 16:48:17 -0300

    create home page component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import { Play } from 'phosphor-react'
 
-// import { Container } from './styles';
+import * as S from './styles';
 
 export function Home() {
   return (
-    <h1>Home</h1>
+    <S.HomeContainer>
+      <form action="">
+        <S.FormContainer>
+          <label htmlFor="task">I will work at</label>
+
+          <S.TaskInput id="task" type="text" placeholder="Give a name to your project" list="task-suggestions" />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+            <option value="Project 4" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+
+          <S.MinutesAmountInput
+            type="number"
+            name=""
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </S.FormContainer>
+
+        <S.CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountDownContainer>
+
+        <S.StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+
+          Start
+        </S.StartCountdownButton>
+      </form>
+    </S.HomeContainer>
   )
 }
